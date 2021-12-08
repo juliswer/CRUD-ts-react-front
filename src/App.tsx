8793b01175d785cd98875ea5a9eb768bf831469d
Julian Swerdlin <julianswer36@gmail.com>
2021-12-09 00:47:42 +0100

    uploading new files

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+interface Props {
+  title: string
+}
+
+function App({title}: Props) {
+  return (
+    <div className="App">
+      <h1>{title}</h1>
+    </div>
+  );
+}
+
+export default App;
