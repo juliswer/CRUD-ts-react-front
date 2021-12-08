337c03039bc163ca039b5050eba373ec035f3038
Julian Swerdlin <julianswer36@gmail.com>
2021-12-09 00:59:05 +0100

    front files

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
+import {useState} from 'react';
 import './App.css';
 
 interface Props {
   title: string
 }
 
+interface Task {
+  id: number;
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
 export function App({title}: Props) {
+
+  const [tasks, setTasks] = useState<Task[]>([
+    {
+      id: 1,
+      title: 'Learn React',
+      description: 'Learn React',
+      completed: false
+    }
+  ])
+
   return (
     <div className="App">
       <h1>{title}</h1>
+
+      {tasks.map(task => (
+        <div>
+          <h2>{task.title}</h2>
+        </div>
+      ))}
+
     </div>
   );
 }
