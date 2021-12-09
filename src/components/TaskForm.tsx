c229dc693ab9e776d83cafa87519d4878f9f2562
Julian Swerdlin <julianswer36@gmail.com>
2021-12-09 01:41:31 +0100

    front files

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,6 +1,19 @@
+import {ChangeEvent, useState} from 'react';
+
 import {AiOutlinePlus} from 'react-icons/ai'
 
 export default function TaskForm() {
+    const [task, setTask] = useState({
+        title: '',
+        description: ''
+    })
+
+    const handleInputChange = ({
+        target: {name, value}
+    }: ChangeEvent<HTMLInputElement|HTMLTextAreaElement>) => {
+        setTask({...task, [name]: value})
+    }
+
   return (
     <div className="card card-body bg-secondary text-dark">
       <h1>Add Task</h1>
@@ -11,6 +24,7 @@ export default function TaskForm() {
           placeholder="Write a title"
           name="title"
           className="form-control mb-3 rounded-0 shadow-none border-0"
+          onChange={handleInputChange}
         />
 
         <textarea
@@ -18,6 +32,7 @@ export default function TaskForm() {
           rows={2}
           placeholder="Write a description"
           className="form-control mb-3 shadow-none border-0"
+          onChange={handleInputChange}
         ></textarea>
 
         <button className="btn btn-primary">
